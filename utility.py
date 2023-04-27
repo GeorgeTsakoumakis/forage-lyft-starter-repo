306fee068536ea1e70dd081ea504c093f401f0bd
George Tsakoumakis <g.tsakoumakis2@newcastle.ac.uk>
2023-04-27 15:06:58 +0100

    Fix

--- utility.py
+++ utility.py
@@ -1,4 +1,3 @@
 def add_years_to_date(original_date, years_to_add):
-    print(original_date)
-    original_date += years_to_add
-    print(original_date)
+    result = original_date.replace(year=original_date.year + years_to_add)
+    return result
